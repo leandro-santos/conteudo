713dc21f6d16a6b16d60095edac4a3b09b54e7d9
Leandro <le.lrdsantos@gmail.com>
2018-08-02 16:37:09 -0300

    inicio - criando api de conteudo

---
+++ src/main/java/com/example/afrase/conteudo/model/Text.java
@@ -0,0 +1,52 @@
+package com.example.afrase.conteudo.model;
+
+import java.util.Date;
+
+public class Text {
+
+    private Integer id;
+    private String description;
+    private String site;
+    private Date update_at;
+    private Date create_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public Date getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Date update_at) {
+        this.update_at = update_at;
+    }
+
+    public Date getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Date create_at) {
+        this.create_at = create_at;
+    }
+}
---
+++ src/main/java/com/example/afrase/conteudo/resource/TextResource.java
@@ -0,0 +1,43 @@
+package com.example.afrase.conteudo.resource;
+
+import com.example.afrase.conteudo.model.Text;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TextResource {
+
+    Map<Integer, Text> texts;
+    public Integer proximoId=1;
+
+    private Text cadastrar(Text text){
+        if (texts == null) {
+            texts = new HashMap<>();
+        }
+        text.setId(proximoId);
+        proximoId++;
+        texts.put(text.getId(), text);
+        return text;
+    }
+
+    @RequestMapping(method= RequestMethod.GET, value="/texts")
+    public void buscar() {
+        System.out.println("chamou!");
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/texts", consumes= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Text> cadastrarText(@RequestBody Text text){
+        Text textCadastrado = cadastrar(text);
+        System.out.println("chamou text post");
+        System.out.println(textCadastrado);
+        return new ResponseEntity<Text>(textCadastrado, HttpStatus.CREATED);
+    }
+}
